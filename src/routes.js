d646480904c0fb4a152cb39de6d11b0676dae284
brunocheble <contato.cheble@gmail.com>
2019-08-14 16:38:48 +0100

    Foi removida a página Main;

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import Main from '~/pages/Main';
 import Service from '~/pages/Service';
 
 const Routes = createAppContainer(createSwitchNavigator({
-  Main,
   Service,
 }));
 

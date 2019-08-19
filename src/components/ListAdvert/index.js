8c5d248e0e58ad3e4564a458d86cd8cd17a680a4
brunocheble <contato.cheble@gmail.com>
2019-08-19 18:22:02 +0100

    Na lista de anúncios, foi adicionada a thumb do anúncio;

--- src/components/ListAdvert/index.js
+++ src/components/ListAdvert/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Moment from 'moment';
 
-import { Linking } from 'react-native';
+import { Linking, Image } from 'react-native';
 import { List, Item, Link, Title, TextLink, Price, Subtitle } from './styles';
 
 const ItemAdvert = ({ data }) => (
   <Item>
     <Link style={data.active ? { backgroundColor: '#54448c' } : { backgroundColor: '#333' }} onPress={() => Linking.openURL(data.link)}>
+      {data.thumb != '' ? (<Image source={{ uri: data.thumb }} style={{ height: 200 }} />) : (<></>)}
       <Title>{data.title}</Title>
-    <Subtitle>
-      <Price>{data.last_price}</Price>
-      <TextLink>{Moment.utc(data.createdAt).format('DD/MM/Y - HH:mm:ss')}</TextLink>
-    </Subtitle>
+      <Subtitle>
+        <Price>{data.last_price}</Price>
+        <TextLink>{Moment.utc(data.createdAt).format('DD/MM/Y - HH:mm:ss')}</TextLink>
+      </Subtitle>
     </Link>
+
   </Item >
 );
 
--- src/components/ListAdvert/styles.js
+++ src/components/ListAdvert/styles.js
@@ -1,25 +1,25 @@
 import styled from 'styled-components/native';
 
 export const List = styled.FlatList.attrs({
-    contentContainerStyle: { paddingHorizontal: 20 },
     showVerticalScrollIndicator: false,
   })`
-  margin-top: 10px;
+  margin-bottom: 1px;
 `;
 
 export const Link = styled.TouchableOpacity`
 border-radius: 4px;
-padding: 10px;
+padding: 20px;
 `
 
 export const Item = styled.View`
-margin-bottom: 10px;
+margin-bottom: 1px;
 `;
 
 export const Title = styled.Text`
 font-size: 14px;
 font-weight: bold;
 color: #fff;
+margin: 10px 0;
 `;
 
 export const TextLink = styled.Text`
@@ -30,7 +30,6 @@ text-align: right;
 `;
 
 export const Subtitle = styled.View`
-margin-top: 10px;
 flex-direction: row;
 justify-content: center;
 align-items: center;

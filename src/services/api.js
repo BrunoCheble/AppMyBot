3ca944b74a1c621e6d1532215665862426308bda
brunocheble <contato.cheble@gmail.com>
2019-08-26 00:14:08 +0100

    Desenvolvimento para testar robôs ativos, pois quando um robô parar de funcionar consulta outro robô;

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,30 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: '',
+});
+
+const getUrlActive = async () => {
+
+  let url = '';
+  let test_url = '';
+
+  for (i = 1; i < 10 && url == ''; i++) {
+    
+    test_url = `https://my-spy-bot${i}.herokuapp.com/`;
+
+    await axios.get(test_url).then((response) => {
+      url = test_url;
+    }).catch((erro) => {});
+
+  }
+
+  return url;
+}
+
+api.interceptors.request.use(async (config) => {
+  config.baseURL = await getUrlActive();  
+  return config;
 });
 
 export default api;
